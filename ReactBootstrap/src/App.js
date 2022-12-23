74ac25d19cbc37be867b34a04425173c8d73fb4b
Thiago Lins <thiago.jorge2001@gmail.com>
2022-12-23 02:26:55 -0300

    Atividade 02 React bootstrap

--- ReactBootstrap/src/App.js
+++ ReactBootstrap/src/App.js
@@ -1,115 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Carousel from 'react-bootstrap/Carousel';
-import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function App() {
+export default function BasicExample() {
   return (
-    <div>
-      <Navbar bg="primary" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Carousel>
-        <Carousel.Item>
+    <Container style={{position: 'relative'}}>
+      <Row style={{position: 'absolute', top: 50, left: 50}}>
+        <Col style={{display:'flex', justifyContent:'center',alignItems:'center', height: 700}}>
           <img
             className="d-block w-100"
-            src="https://cdn.pixabay.com/photo/2015/09/17/10/31/banner-943868_960_720.jpg"
+            src="https://www.goldmansachs.com/insights/pages/multimedia/the-future-of-coding-is-conversation.jpg"
             alt="First slide"
-            width={960}
-            height={480}
+            width={1000}
+            height={780}
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://i.pinimg.com/originals/30/4b/0b/304b0ba836f045e4fc04cf8f6391e991.jpg"
-            alt="Second slide"
-            width={960}
-            height={480}
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://cdn.pixabay.com/photo/2015/11/19/08/51/banner-1050624_960_720.jpg"
-            alt="Third slide"
-            width={960}
-            height={480}
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-    </div>
+        </Col>
+        <Col style={{display:'flex', justifyContent:'center',alignItems:'center', height: 700}}>
+          <Form style={{borderWidth: 1, borderStyle:'solid', borderRadius: 5,padding: 20, height: 400}}>
+            <h1 style={{textAlign: 'center'}}>
+              Login
+            </h1>
+            <div style={{position: 'relative', top: 50}}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type="password" placeholder="" />
+              </Form.Group>
+              <Button style={{ width: 300 }} variant="primary" type="submit">Salvar </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
-export default App;
